0e9993222073ff1d252a6601838de8548f3ea026
S-Amier <s.a.sd@outlook.com>
2025-03-19 16:54:58 +0100

    Start Chapter 3
    First 2 files created about lists

---
+++ python_work/H03 - Introducing Lists/bicycles.py
@@ -0,0 +1,16 @@
+# List of bicycles, returning first value
+bicycles = ['trek', 'cannondale', 'redline', 'specialised']
+print(bicycles[0].title())
+
+# Getting last item of list
+print(bicycles[-1])
+
+# Getting second to last item of list
+print(bicycles[-2])
+
+# Using individual value from list in a sentence
+message = f"My first bicycle was a {bicycles[1].title()}"
+
+# Returning sentence
+print(message)
+
---
+++ python_work/H03 - Introducing Lists/motorcycles.py
@@ -0,0 +1,25 @@
+# Modifying Elements in a list
+
+# List of motorcycles, return value
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# Modify first value to be ducati instead of honda
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding (append) item to the back of the list
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+# Assigning (inserting) item to spot inside list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print (motorcycles)
